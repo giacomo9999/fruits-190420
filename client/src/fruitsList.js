30260e41b71619172f0ddb677a7707935c00b806
James Gary <gmarchesi888@gmail.com>
2019-04-20 08:58:15 -0400

    Added FruitsLost component.

---
+++ client/src/fruitsList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class FruitsList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Fruits List Goes Here</h1>
+      </div>
+    );
+  }
+}
+export default FruitsList;
